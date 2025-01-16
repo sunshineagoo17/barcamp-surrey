531861c9dfd8aa075385cb814ba7380f50c253f0
sunshineagoo17 <sunshine.agoo@gmail.com>
2025-01-16 00:16:32 -0500

    Added hero animation

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,19 +1,56 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Hero.scss';
-import Logo from "../../assets/logos/barcamp_logo-main-gradient-logo.png"; 
+import Logo from "../../assets/logos/barcamp_logo-main-gradient-logo.png";
 
 const Hero = () => {
+  const [currentPhraseIndex, setCurrentPhraseIndex] = useState(0);
+  const [isFading, setIsFading] = useState(false);
+
+  const phrases = [
+    '"Unconference"',
+    '"Open Forum"',
+    '"Collaborative Summit"',
+    '"Idea Exchange"',
+    '"Community Jam"',
+    '"Innovation Gathering"',
+    '"Interactive Meetup"',
+    '"Knowledge Share"',
+    '"Peer-Led Event"',
+  ];
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIsFading(true); 
+
+      setTimeout(() => {
+        setCurrentPhraseIndex((prevIndex) => (prevIndex + 1) % phrases.length);
+        setIsFading(false);  
+      }, 500);  
+
+    }, 3000);  // Total cycle every 3 seconds
+
+    return () => clearInterval(interval);
+  }, []);
+
   const scrollToAbout = () => {
     document.getElementById('about').scrollIntoView({ behavior: 'smooth' });
   };
 
   return (
     <section className="hero">
-      <img src={Logo} alt="BarCamp Surrey Logo" className="hero__logo" />
-      <h1 className="hero__title">A Technology <span className="hero__highlight">"Unconference"</span> in the Heart of Surrey</h1>
-      <button className="hero__button" onClick={scrollToAbout}>
-        Tell Me More
-      </button>
+             <img src={Logo} alt="BarCamp Surrey Logo" className="hero__logo" />
+        <div className='hero__copy-container'>
+            <h1 className="hero__title">
+                A Technology <span className={`hero__highlight ${isFading ? 'fade' : ''}`}>
+                {phrases[currentPhraseIndex]}
+                </span>{" "}
+                in the Heart of Surrey
+            </h1>
+            <button className="hero__button" onClick={scrollToAbout}>
+                Tell Me More
+            </button>
+        </div>
+ 
     </section>
   );
 };
