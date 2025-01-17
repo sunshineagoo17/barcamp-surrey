d93106aef667d4bfe57c79ee35b5bbc14aa828eb
sunshineagoo17 <sunshine.agoo@gmail.com>
2025-01-16 21:20:04 -0500

    Cleaned up footer

--- src/components/Registration/Registration.jsx
+++ src/components/Registration/Registration.jsx
@@ -7,17 +7,20 @@ const Registration = () => {
   return (
     <section id='register' className='registration fade-in'>
       <h2 className='registration__title fade-in'>Register Now</h2>
-      <iframe
-        src='https://lu.ma/embed/event/evt-u6LyCSDe4NG1hBq/simple'
-        width='600'
-        height='450'
-        frameBorder='0'
-        style={{ border: '1px solid #bfcbda88', borderRadius: '4px' }}
-        allowFullScreen
-        aria-hidden='false'
-        tabIndex='0'
-        title='Barcamp Surrey Registration Form'
-        className='fade-in'></iframe>
+      <div className='registration__container'>
+        <iframe
+            src='https://lu.ma/embed/event/evt-u6LyCSDe4NG1hBq/simple'
+            width='600'
+            height='450'
+            frameBorder='0'
+            style={{ border: '1px solid #bfcbda88', borderRadius: '4px' }}
+            allowFullScreen
+            aria-hidden='false'
+            tabIndex='0'
+            title='Barcamp Surrey Registration Form'
+            className='fade-in'>
+        </iframe>
+       </div>
     </section>
   );
 };
