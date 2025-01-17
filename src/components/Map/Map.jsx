8038225a224d8f52d8f3bf6bf47d18af7d779998
sunshineagoo17 <sunshine.agoo@gmail.com>
2025-01-17 12:16:48 -0500

    Changed the map to the right location

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -9,11 +9,16 @@ const Map = () => {
       <h2 className='map__title fade-in'>How to Get There</h2>
       <div className='map__container fade-in'>
         <iframe
-          title='Godalming Train Station'
-          src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2471.5502305943773!2d-0.6189953842279347!3d51.18425957958595!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4875d40178d8a3ef%3A0x8f07de1762ee7c6f!2sGodalming%20Station!5e0!3m2!1sen!2suk!4v1614011633391!5m2!1sen!2suk'
+          title='Godalming College'
+          src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2471.978194090168!2d-0.6196663!3d51.176197!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4875ce283941035f%3A0x34b2d40c0fc0d44a!2sGodalming%20College!5e0!3m2!1sen!2suk!4v1705485430000!5m2!1sen!2suk'
+          width='600'
+          height='450'
+          style={{ border: 0 }}
           allowFullScreen=''
           loading='lazy'
-          className='map__iframe'></iframe>
+          referrerPolicy='no-referrer-when-downgrade'
+          className='map__iframe'
+        ></iframe>
       </div>
     </section>
   );
