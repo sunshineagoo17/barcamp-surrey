5242afb2658d06f4463d95643772ccf93e2b3a00
sunshineagoo17 <sunshine.agoo@gmail.com>
2025-01-17 22:37:23 -0500

    Fixed iframe

--- src/components/Registration/Registration.jsx
+++ src/components/Registration/Registration.jsx
@@ -4,23 +4,34 @@ import useScrollAnimation from '../../hooks/useScrollAnimation';
 const Registration = () => {
   useScrollAnimation();
 
+  const eventUrl = 'https://lu.ma/ins7ivwr?from=embed'; 
+
   return (
     <section id='register' className='registration fade-in'>
-      <h2 className='registration__title fade-in'>Register Now</h2>
-      <div className='registration__container'>
+      <h2 className='registration__title fade-in'>Ready to Join Us?</h2>
+
+      <button
+        className='registration__button'
+        onClick={() => window.open(eventUrl, '_blank')}
+      >
+        Save Your Spot
+      </button>
+
+      <div
+        className='registration__container'
+        onClick={() => window.open(eventUrl, '_blank')}
+        role='button'
+        tabIndex={0}
+      >
         <iframe
-            src='https://lu.ma/embed/event/evt-u6LyCSDe4NG1hBq/simple'
-            width='600'
-            height='450'
-            frameBorder='0'
-            style={{ border: '1px solid #bfcbda88', borderRadius: '4px' }}
-            allowFullScreen
-            aria-hidden='false'
-            tabIndex='0'
-            title='Barcamp Surrey Registration Form'
-            className='fade-in'>
-        </iframe>
-       </div>
+          src='https://lu.ma/embed/event/evt-u6LyCSDe4NG1hBq/simple'
+          allowFullScreen
+          aria-hidden='false'
+          tabIndex='0'
+          title='Barcamp Surrey Registration Form'
+          className='registration__iframe fade-in'
+        />
+      </div>
     </section>
   );
 };
