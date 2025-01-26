25db1333c421e0001a44ae9f388241806a954bd3
sunshineagoo17 <sunshine.agoo@gmail.com>
2025-01-26 02:01:30 -0500

    Added register button to other pages

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -18,75 +18,82 @@ const Header = () => {
   const isMainPage = location.pathname === '/';
   const isSharePage = location.pathname === '/share';
   const isFAQPage = location.pathname === '/faq';
+  const isKnownPage = isMainPage || isFAQPage || isSharePage;
+  const is404Page = !isKnownPage; 
 
   return (
-    <header className='header'>
+    <header className="header">
       {/* Logo Image */}
-      <div className='header__logo'>
-        <Link to='/'>
-          <img
-            src={Logo}
-            alt='BarCamp Surrey Logo'
-            className='header__logo-img'
-          />
+      <div className="header__logo">
+        <Link to="/">
+          <img src={Logo} alt="BarCamp Surrey Logo" className="header__logo-img" />
         </Link>
       </div>
 
       {/* Navigation Links */}
-      <nav className='header__nav'>
+      <nav className="header__nav">
         {isMainPage ? (
           <>
-            <a href='#about' className='header__nav-link'>
+            <a href="#about" className="header__nav-link">
               About
             </a>
-            <a href='#faq' className='header__nav-link'>
+            <a href="#faq" className="header__nav-link">
               FAQ
             </a>
-            <a href='#register' className='header__nav-link'>
+            <a href="#register" className="header__nav-link">
               Register
             </a>
-            <a
-              href='mailto:Laura.czajkowski@gmail.com'
-              className='header__nav-link'>
+            <a href="mailto:Laura.czajkowski@gmail.com" className="header__nav-link">
               Contact
             </a>
           </>
         ) : (
           <>
-            <Link to='/' className='header__nav-link'>
+            <Link to="/" className="header__nav-link">
               Home
             </Link>
             {!isSharePage && (
-              <Link to='/share' className='header__nav-link'>
+              <Link to="/share" className="header__nav-link">
                 Share
               </Link>
             )}
             {!isFAQPage && (
-              <Link to='/faq' className='header__nav-link'>
+              <Link to="/faq" className="header__nav-link">
                 FAQ
               </Link>
+            )}
+            {(isFAQPage || isSharePage || is404Page) && (
+              <a
+                href="https://lu.ma/ins7ivwr?from=embed"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="header__nav-link header__nav-link--highlight"
+              >
+                Register
+              </a>
             )}
           </>
         )}
       </nav>
 
       {/* Light/Dark Mode Toggle */}
-      <div className='header__toggle-wrapper'>
+      <div className="header__toggle-wrapper">
         <button
           onClick={toggleTheme}
           className={`header__toggle ${
             theme === 'light' ? 'header__toggle--light' : 'header__toggle--dark'
-          }`}>
-          <div className='header__toggle-switch'>
+          }`}
+        >
+          <div className="header__toggle-switch">
             {theme === 'light' ? (
               <FontAwesomeIcon
                 icon={faSun}
-                className='header__toggle-icon header__toggle-icon--sun'
+                className="header__toggle-icon header__toggle-icon--sun"
               />
             ) : (
               <FontAwesomeIcon
                 icon={faMoon}
-                className='header__toggle-icon header__toggle-icon--moon'
+                className="header__toggle-icon header__toggle-icon--moon"
               />
             )}
           </div>
